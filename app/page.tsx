f6e49bb25c0c35729ef06d37185c812dc859ba01
PiyushKanungo <piyushkanungo34@gmail.com>
2025-03-27 12:51:21 +0530

    cool hunter commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 export default function Home() {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-50">
-        <h1 className="text-4xl font-bold text-indigo-600">
-          Welcome to piyushkanungo.com (Coming Soon)!
-        </h1>
-      </div>
+      <main
+        className="min-h-screen bg-cover bg-center flex items-center justify-center relative"
+        style={{ backgroundImage: "url('/images/bghunter.jpeg')" }}
+      >
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="relative max-w-4xl text-center px-4">
+          <p className="text-3xl md:text-4xl font-semibold text-white italic drop-shadow-lg">
+            {`"You Should Enjoy the Little Detours to the Fullest Because That's Where You'll Find Things More Important Than What You Want"`}
+          </p>
+          <p className="mt-4 text-xl text-gray-300 font-medium">- Ging Freecss</p>
+        </div>
+      </main>
     );
   }
   
