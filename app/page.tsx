dd22264ef4b0fe6a0c0b11722b42f153f3fe9de8
PiyushKanungo <piyushkanungo34@gmail.com>
2025-03-27 11:38:36 +0530

    Prepare for initial deployment

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <h1 className="text-4xl font-bold text-indigo-600">
+          Welcome to piyushkanungo.com (Coming Soon)!
+        </h1>
+      </div>
+    );
+  }
+  
